3d7c123966745fa470255a4dd0458f8138fff072
Marina Omelcenko <98337926+mrn2022mlcn@users.noreply.github.com>
2022-08-07 21:02:14 +0300

    tasks 8-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButton = document.querySelector('button.change-color');
+console.log(changeButton);
+const body = document.querySelector('body');
+console.log(body);
+const colorValue = document.querySelector('span.color');
+console.log(colorValue);
+
+
+changeButton.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+})
